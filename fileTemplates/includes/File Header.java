61fbe8bb765c7477c39f1ab735ca419c4f712aab
dertroglodyt <dertroglodyt@gmail.com>
2017-08-27 12:12:51 +0200

    AI-171.4294784 <dertroglodyt@trog-rig Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, trainingPlugin.xml, project.default.xml, diff.xml, other.xml, applicationLibraries.xml, Default.xml, androidStudioFirstRun.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, androidEditors.xml, gradle.run.settings.xml, code.style.schemes.xml, colors.scheme.xml, notifications.xml, scala_config.xml, abbreviations.xml, NetBeans copy.xml, git.xml, github_settings.xml, customization.xml, Default.xml, scala.xml, jdk.table.xml, laf.xml, debugger.renderers.xml, Trog.icls, Default _1_.xml, web-browsers.xml, macros.xml, vcs.xml, editor.codeinsight.xml, toStringTemplates.xml, Darcula Trog.icls, editor.rich.copy.xml, editor.xml, hg.xml, debugger.xml, path.macros.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,8 @@
+#set ($USER = "DerTroglodyt")
+#set ($COMPANY = "HDC, Germany")
+#set ($EMAIL = "dertroglodyt@gmail.com")
+/**
+ * Created by ${USER} on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}.
+ * Email ${EMAIL}
+ * Copyright by ${COMPANY}
+ */
